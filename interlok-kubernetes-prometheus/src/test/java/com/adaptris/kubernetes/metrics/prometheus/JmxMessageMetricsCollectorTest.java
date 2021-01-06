5e33d58ca38cca322e7e4c91344e8fbe3021853a
Sebastien Belin <sebastien.belin@adaptris.com>
2021-01-06 06:20:43 +0000

    INTERLOK-3403 #comment Upgraded to mockito 3.7.0
    Remove dependency on junit 4. If we use junit 5 there is no need to keep it.

--- interlok-kubernetes-prometheus/src/test/java/com/adaptris/kubernetes/metrics/prometheus/JmxMessageMetricsCollectorTest.java
+++ interlok-kubernetes-prometheus/src/test/java/com/adaptris/kubernetes/metrics/prometheus/JmxMessageMetricsCollectorTest.java
@@ -1,11 +1,7 @@
 package com.adaptris.kubernetes.metrics.prometheus;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static org.mockito.Matchers.any;
-
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.never;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,6 +17,7 @@ import javax.management.ObjectInstance;
 import javax.management.ObjectName;
 import javax.management.QueryExp;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -33,73 +30,78 @@ import com.adaptris.core.interceptor.MessageStatistic;
 public class JmxMessageMetricsCollectorTest {
 
   private JmxMessageMetricsCollector collector;
-  
+
   @Mock private MBeanServer mockMBeanServer;
-  
+
   private Set<ObjectInstance> objectSet;
-  
+
   @Mock private ObjectInstance mockObjectInstance;
-  
+
   @Mock private ObjectName mockObjectName;
-  
+
   private ObjectName realObjectName;
-  
-  @Mock private MessageMetricsStatisticsMBean mockMMSMBean; 
-  
+
+  @Mock private MessageMetricsStatisticsMBean mockMMSMBean;
+
   private List<MessageStatistic> messagesStatisticsList;
-  
+
   @Mock MessageMetricsListener mockListener;
-  
+
+  private AutoCloseable closeable;
+
   @BeforeEach
   public void setUp() throws Exception {
-    MockitoAnnotations.initMocks(this);
-    
+    closeable = MockitoAnnotations.openMocks(this);
+
     collector = new JmxMessageMetricsCollector();
     collector.setInterlokMBeanServer(mockMBeanServer);
-    
+
     objectSet = new HashSet<>();
     objectSet.add(mockObjectInstance);
-    
+
     realObjectName = new ObjectName("com.adaptris:type=Metrics,adapter=adapter,channel=channel,workflow=workflow");
-    
-    messagesStatisticsList = new ArrayList<MessageStatistic>();
-    
+
+    messagesStatisticsList = new ArrayList<>();
+
     when(mockMBeanServer.queryMBeans(any(ObjectName.class), any(QueryExp.class)))
-        .thenReturn(objectSet);
-    
+    .thenReturn(objectSet);
+
     when(mockMMSMBean.getStatistics())
-        .thenReturn(messagesStatisticsList);
+    .thenReturn(messagesStatisticsList);
   }
-  
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    closeable.close();
+  }
+
   @Test
   public void testMBeansLoadedFirstExecution() {
-    when(mockObjectInstance.getClassName())
-        .thenReturn(MessageMetricsStatistics.class.getName());
-    when(mockObjectInstance.getObjectName())
-        .thenReturn(mockObjectName);
-    
+    when(mockObjectInstance.getClassName()).thenReturn(MessageMetricsStatistics.class.getName());
+    when(mockObjectInstance.getObjectName()).thenReturn(mockObjectName);
+
     try {
       collector.run();
     } catch (Throwable t) {};
-    
-    verify(mockMBeanServer).queryMBeans(any(ObjectName.class), any(QueryExp.class));
+
+    verify(mockMBeanServer).queryMBeans(any(ObjectName.class), isNull());
   }
-  
-  @Test 
+
+  @Test
   public void testNotifyNewStats() throws Exception {
-    Map<ObjectName, MessageMetricsStatisticsMBean> mbeans = new HashMap<ObjectName, MessageMetricsStatisticsMBean>();
+    Map<ObjectName, MessageMetricsStatisticsMBean> mbeans = new HashMap<>();
     mbeans.put(realObjectName, mockMMSMBean);
-    
+
     collector.registerListener(mockListener);
-    
+
     collector.setMetricsMBeans(mbeans);
     collector.run();
-    
+
     collector.deregisterListener(mockListener);
-    
+
     Thread.sleep(500);
-    
+
     verify(mockListener).notifyMessageMetrics(any());
   }
-  
+
 }
